f0c34ec7f019be78a52be19a16bef8d08500a0c8
ianriley237 <Ian.Riley237@gmail.com>
2023-11-16 12:57:44 -0600

    Create Worker thread to run sim
    simulator.js is run on its own thread

--- js/interface.js
+++ js/interface.js
@@ -1,37 +1,100 @@
+var worker;
+var $objects = [];
 $(function() {
-    $("#reset-simulation-button").on("click", function () {
-        celestialObjects = [];
-        clear();
-        pause();
-    });
-    $("#create-defaults-button").on("click", function () {
-        celestialObjects.push(...createDefaultCelestialObjects());
-    });
-    $("#play-simulation-button").on("click", function () {
-        play();
-    });
-    $("#pause-simulation-button").on("click", function () {
-        pause();
-    });
+    createSimulation();
+});
+function createSimulation() {
+    if (typeof(Worker) == "undefined") {
+      console.log("No Web Worker support.");
+      return;
+    }
+    if (typeof (worker) == "undefined") {
+        worker = new Worker("js/simulator.js");
+    }
+    worker.onmessage = (event) => {
+        switch (event.data[0]) {
+            case 'apply':
+                event.data.slice(1).forEach(applyObjects)
+                worker.postMessage(["continue"]);
+                break;
+            default:
+                break;
+        }
+    };
+}
 
-    $('#create-object').toggle();
-    $('#field').on("click", function (e) {
-        $('#create-object').toggle().css({
-            'left':e.pageX - $('#create-object').width() / 2, 
-            'top':e.pageY - $('#create-object').height()
-        });
-        $("#create-object-x:text").val(e.pageX);
-        $("#create-object-y:text").val(e.pageY);
-    })
-    $("#create-object-button").on("click", function () {
-        $('#create-object').toggle();
-        var pos = new Vector();
-        pos[0] = parseFloat($("#create-object-x:text").val() || 0);
-        pos[1] = parseFloat($("#create-object-y:text").val() || 0);
-        var vel = new Vector();
-        vel[0] = parseFloat($("#create-object-dx:text").val() || 0);
-        vel[1] = parseFloat($("#create-object-dy:text").val() || 0);
-        var color = $("#color-input").val();
-        celestialObjects.push(createObject(pos, vel, 2, color));
+function killSimulation() {
+    if (typeof (worker) == "undefined") {
+        return;
+    }
+    worker.terminate();
+    worker = undefined;
+}
+
+function applyObjects(object, index) {
+    if ($objects[index] != undefined) {
+        $("#body" + index).css({
+            "left": object.xPos, // TODO: factor in width and height
+            "top": object.yPos
+        })
+    } else {
+        $objects.push($("<div></div>"));
+        $("#field").append(
+            $objects[index].attr("id", "body" + index).attr("class", "body").css({
+                "borderRadius": "100%",
+                "backgroundColor": object.color,
+                "left": object.xPos, // TODO: factor in width and height
+                "top": object.yPos,
+            })//.draggable()
+        );
+    }
+}
+
+$("#create-defaults-button").on("click", () => {
+    worker.postMessage(["default"]);
+});
+
+$("#reset-simulation-button").on("click", () => {
+    killSimulation();
+    createSimulation();
+    $objects.forEach(element => {
+        element.remove();
     });
+    $objects = [];
+});
+
+$("#play-simulation-button").on("click", () => {
+    worker.postMessage(["play"]);
+});
+
+$("#pause-simulation-button").on("click", () => {
+    worker.postMessage(["pause"]);
+});
+
+
+$('#field').on("click", (e) => {
+    $('#create-object').toggle().css({
+        'left':e.pageX - $('#create-object').width() / 2, 
+        'top':e.pageY - $('#create-object').height()
+    });
+    $("#create-object-x:text").val(e.pageX);
+    $("#create-object-y:text").val(e.pageY);
+})
+
+$("#create-object-button").on("click", () => {
+    $('#create-object').toggle();
+    let xPos = parseFloat($("#create-object-x:text").val() || 0);
+    let yPos = parseFloat($("#create-object-y:text").val() || 0);
+    let xVel = parseFloat($("#create-object-dx:text").val() / 10000 || 0);
+    let yVel = parseFloat($("#create-object-dy:text").val() / 10000 || 0);
+    var color = $("#color-input").val();
+    worker.postMessage([
+        "object",
+        [xPos, yPos, xVel, yVel, 2, 1, color]]);
+});
+
+$("#resolutionInput").on("change", () => {
+    worker.postMessage([
+        "resolution",
+        9999 - $("#resolutionInput").val() || '0']);
 });
--- js/simulator.js
+++ js/simulator.js
@@ -1,30 +1,68 @@
-/// <reference path="celestial_object.js" />
+class Vector extends Array {
+    add(otherVector) {
+        return this.map((e, i) => e + otherVector[i]);
+    }
+    sub(otherVector) {
+        return this.map((e, i) => e - otherVector[i]);
+    }
+    multiply(multiplier) {
+        return this.map((e, i) => e * multiplier);
+    }
+    magnitude() {
+        return (this[0] ** 2 + this[1] ** 2) ** 0.5
+    }
+    normalize() {
+        return this.map((e) => e / this.magnitude());
+    }
+    getDistance(otherVector) {
+        return Math.abs(this.sub(otherVector).magnitude());
+    }
+}
+
+class CelestialObject {
+    pos;
+    vel;
+    mass;
+    radius;
+    color;
+    constructor(pos, vel, mass, radius, color) {
+        this.pos = pos;
+        this.vel = vel;
+        this.mass = mass;
+        this.radius = radius;
+        this.color = color;
+    }
+    getDistance(other) {
+        return this.pos.getDistance(other.pos);
+    }
+    applyTranslation() {
+        this.pos = this.pos.add(this.vel);
+    }
+    toArray() {
+        return {xPos: this.pos[0], yPos: this.pos[1], xVel: this.vel[0], yVel: this.vel[1], mass: this.mass, radius: this.radius, color: this.color};
+    }
+}
+
 var GFactorOffset = 6;
-const G = 6.67430 * 10**(-11 + GFactorOffset);
+const G = 6.67430 * 10 ** (-11 + GFactorOffset);
 
 var celestialObjects = [];
 var currObj = 0;
 var totalMassOverRadiusSquared = 0;
 var totalVector = new Vector(0, 0);
+var playing = 0;
+var resolution = 500
 
-var isPaused = 1;
 function play() {
-    isPaused = 0;
-    var id = null;
-    clearInterval(id);
-    id = setInterval(frame, 1);
-    function frame() {
-        if (isPaused == 1) {
-            return;
-        }
-        for (var i = 0; i < 500; i++) {
+    if (playing) {
+        for (let i = 0; i <= resolution; i++)
             celestialObjects.forEach(applyForcesToOthers);
-        }
+        var returnObjects = ["apply"];
+        celestialObjects.forEach(object => {
+            returnObjects.push(object.toArray()) // Add index probably
+        });
+        self.postMessage(returnObjects);
     }
-}
-
-function pause () {
-    isPaused = 1;
 }
 
 function applyForcesToOthers(ob, index) {
@@ -43,9 +81,57 @@ function findValues(ob, index) {
     if (index == currObj || celestialObjects[currObj].getDistance(ob) == 0) {
         return;
     }
-    totalMassOverRadiusSquared += ob.mass / celestialObjects[currObj].getDistance(ob)**2;
-    totalVector = 
+    totalMassOverRadiusSquared += ob.mass / celestialObjects[currObj].getDistance(ob) ** 2;
+    totalVector =
         totalVector.add(
             ob.pos.sub(celestialObjects[currObj].pos).normalize()
         );
+}
+
+
+onmessage = function (e) {
+    switch (e.data[0]) {
+        case "object":
+            e.data.slice(1).forEach(data => {
+                addObject(data);
+            });
+            break;
+        case "default":
+            addObject([160, 160, 0.0005, 0.0005, 2, 1, "red"]);
+            addObject([320, 320, -0.001, 0.001, 2, 1, "blue"]);
+            addObject([400, 220, 0.000, 0.0005, 2, 1, "green"]);
+            addObject([600, 220, -0.000, -0.0005, 2, 1, "yellow"]);
+            break;
+        case "play":
+            playing = 1;
+            play();
+            break;
+        case "continue":
+            play();
+            break;
+        case "pause":
+            playing = 0;
+            break;
+        case "resolution":
+            resolution = e.data[1];
+            break;
+        default:
+            break;
+    }
+};
+
+function addObject(data) {
+    let newObject = new CelestialObject(
+        pos = new Vector(data[0], data[1]),
+        vel = new Vector(data[2], data[3]),
+        mass = data[4],
+        radius = data[5],
+        color = data[6],
+    )
+    celestialObjects.push(newObject);
+    let returnObjects = ["apply"];
+    celestialObjects.forEach(object => {
+        returnObjects.push(object.toArray()) // Add index probably
+    });
+    self.postMessage(returnObjects);
 }
